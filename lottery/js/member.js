eeac4edafeade84068c132742ea5470e722dbd7f
Make <815432837@qq.com>
2020-06-25 22:35:55 +0800

    抽奖程序

---
+++ lottery/js/member.js
@@ -0,0 +1,114 @@
+var member = [
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.2'
+  },
+  {
+    name:'阿航',
+    phone:'No.3'
+  },
+  {
+    name:'阿航',
+    phone:'No.4'
+  },
+  {
+    name:'阿航',
+    phone:'No.5'
+  },
+  {
+    name:'阿航',
+    phone:'No.6'
+  },  {
+    name:'阿航',
+    phone:'No.7'
+  },
+  {
+    name:'阿航',
+    phone:'No.8'
+  },
+  {
+    name:'阿航',
+    phone:'No.9'
+  },  {
+    name:'阿航',
+    phone:'No.10'
+  },
+  {
+    name:'阿航',
+    phone:'No.11'
+  },
+  {
+    name:'阿航',
+    phone:'No.12'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+  {
+    name:'阿航',
+    phone:'No.1'
+  },
+]
